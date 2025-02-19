4def8a281d6f9b6a85d89b8934b099dc4bd65cf1
e=mc2 <86334344+yencvt@users.noreply.github.com>
2025-02-19 23:29:49 +0700

    initial

--- common/obj/Debug/net8.0/common.AssemblyInfo.cs
+++ common/obj/Debug/net8.0/common.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f176b8445ab721f0529db146d107b8413256aea7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3bd69d7b2e23d48e92b0e740267c5103ff0fd98")]
 [assembly: System.Reflection.AssemblyProductAttribute("common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
